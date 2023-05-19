a804de03dbc3949332c9cbe2903efad75303220d
joelluke <joelluke@Imacs-iMac.station>
2023-05-19 16:34:40 +0100

    working on routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,23 @@
 import "./App.css";
-import Title from "./components/Title";
-import Todo from "./components/Todo";
-import Modal from "./components/Modal";
-import Cart from "./components/Counter";
-import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+
 
 function App() {
   
-  const [showModal, setShowModal] = useState(false);
-
-  function onTodoDelete() {
-    setShowModal(true)
-  }
-
-  function confirmModal() {
-    console.log("confirm")
-    setShowModal(false)
-  }
-
-  function cancelModal() {
-    console.log("cancel")
-    setShowModal(false)
-  }
-
-
-
-
   return (
     <>
-      <Title />
-      <div>
-        <input
-          type="text"
-          onChange={(event) => {
-            console.log(event.target.value);
-          }}
-        />
-        <button onClick={() => setShowModal(true)}>Add todo</button>
-      </div>
-      <div className="todo__wrapper">
-        <Todo deleteTodo={onTodoDelete} title="Finish Frontend Simplified" />
-        <Todo deleteTodo={onTodoDelete} title="Finish Interview Section" />
-        <Todo deleteTodo={onTodoDelete} title="Land a $100k Job" />
-      </div>
-      {showModal && <Modal confirm={confirmModal} cancel={cancelModal} question="Are you sure?" />}
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
     </>
   );
 }
