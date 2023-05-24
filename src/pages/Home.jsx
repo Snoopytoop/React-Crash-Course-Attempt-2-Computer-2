42ead0d30b22e287c31c1258e4f00198d9673aa7
joelluke <joelluke@Imacs-iMac.station>
2023-05-24 18:01:05 +0100

    more work on api integration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Home() {
+  const [users, setUsers] = useState([]);
+
   async function fetchUsers() {
     const { data } = await axios.get(
       "https://jsonplaceholder.typicode.com/users"
     );
+    setUsers(data);
     console.log(data);
   }
 
   useEffect(() => {
-    fetchUsers();
+    setTimeout(() => {
+      fetchUsers();
+    }, 500);
   }, []);
+
+  const pixels = "3px";
+
   return (
     <>
-      <h1>Home</h1>
+      {users.map((user) => (
+        <div key={user.id} style={{ border: `${pixels} solid black` }}>
+          <div>{user?.id}</div>
+          <div>{user?.name}</div>
+          <div>{user?.email}</div>
+          <div>{user?.username}</div>
+        </div>
+      ))}
     </>
   );
 }
